65baf0437ac92233c81a2d61b16f163daf253860
Chathra Navoda <chathra96@gmail.com>
2022-10-13 17:31:54 +0530

    Test with route params

--- app.js
+++ app.js
@@ -11,3 +11,11 @@ app.listen(8000);
 app.get("/test", (req, res) => {
   res.send("Moving to the test route");
 });
+
+//route parameters
+/*Route params are typically used for info that can't be ignored. After executing the program the "(req.params)"
+ will print out the object and the value that contains the object.  */
+app.get("/test/:name/:age", (req, res) => {
+  console.log(req.params);
+  res.send("Test with route params");
+});
